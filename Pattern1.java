3248b0a00f850b9938d6896538414a15899b5edc
snekaC2024 <sneka202420@gmail.com>
2025-06-15 09:58:18 +0530

    First commit

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		pattern1(n);
+	}
+	
+	static void pattern1(int n) {
+
+		for(int i =1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
